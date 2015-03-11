52584ddb44a0e8b35670f68543e452a441f94d16
patyRock <linh17tp@gmail.com>
2015-03-12 01:12:16 +0700

    pust day  6 successful

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,24 +1,18 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.screens.GameScreen;
 import com.mygdx.game.utils.AssetsLoader;
 
 public class MyGdxGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
 	public void create () {
         AssetsLoader.load();
         setScreen(new GameScreen());
 	}
 
-	@Override
-	public void render () {
-	}
+
     @Override
     public void dispose() {
         super.dispose();
